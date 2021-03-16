f5bcbe71fe78ddd7c591b98741216e8ad80addc7
ishadwivedi <74676933+ishadwivedi@users.noreply.github.com>
2021-03-17 01:04:40 +0530

    Update arith.py

--- arith.py
+++ arith.py
@@ -1,1 +1,16 @@
-print("g")
+
+def arith(x, y):
+    
+
+    x = int(input("Enter value of a: "))
+    y = int(input("Enter value of b: "))
+    a = x+y
+    print(a)
+    b = x-y
+    print(b)
+    c = x*9
+    print(c)
+    d = x/y
+    return d
+
+arith()
